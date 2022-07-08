5f7e0a580cc058f6aa3eed7f60b3ce61b1726a42
AntonKulagin <i-gorusich@mail.ru>
2022-07-08 18:36:01 +0300

    corrected task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Users from './components/users';
+import Users from './app/components/users';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';

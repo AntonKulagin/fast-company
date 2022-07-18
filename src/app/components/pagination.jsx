90c2fa7b85810c17e50da70a716bf964a2d508d4
AntonKulagin <i-gorusich@mail.ru>
2022-07-18 22:57:14 +0300

    task pagination done

---
+++ src/app/components/pagination.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import _ from "lodash";
+import PropTypes from "prop-types";
+
+const Pagination = ({
+    itemsCount,
+    pageSize,
+    onChangePage,
+    onPrevNextPage,
+    currentPage
+}) => {
+    const pageCount = Math.ceil(itemsCount / pageSize);
+    const pages = _.range(1, pageCount + 1);
+
+    return (
+        <>
+            {pageCount > 1 && (
+                <nav>
+                    <ul className="pagination">
+                        <li className="page-item">
+                            <button
+                                className="page-link"
+                                onClick={() =>
+                                    onPrevNextPage("prev", pages.length)
+                                }
+                            >
+                                Previous
+                            </button>
+                        </li>
+                        {pages.map((page) => (
+                            <li
+                                className={
+                                    "page-item" +
+                                    (currentPage === page ? " active" : "")
+                                }
+                                key={"page_" + page}
+                            >
+                                <button
+                                    className="page-link"
+                                    onClick={() => onChangePage(page)}
+                                >
+                                    {page}
+                                </button>
+                            </li>
+                        ))}
+
+                        <li className="page-item">
+                            <button
+                                className="page-link"
+                                onClick={() =>
+                                    onPrevNextPage("next", pages.length)
+                                }
+                            >
+                                Next
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            )}
+        </>
+    );
+};
+Pagination.propTypes = {
+    itemsCount: PropTypes.number.isRequired,
+    pageSize: PropTypes.number.isRequired,
+    onChangePage: PropTypes.func.isRequired,
+    onPrevNextPage: PropTypes.func.isRequired,
+    currentPage: PropTypes.number.isRequired
+};
+
+export default Pagination;
